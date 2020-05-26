639b6cf6fb9bbf032d5028813aa6a624ba96a5ab
Louis <thecodex@outlook.fr>
2020-05-26 17:50:07 +0200

    Ajout d'un script de launch.sh
    Script bash permettant de lancer les scripts python en une ligne

---
+++ launch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+nohup python3 credentials.py
+nohup python3 creeper.py
+nohup python3 liste-serveurs.py
+nohup python3 rpg_paradize.py
+nohup python3 top-serveur.py
+nohup python3 serveur-prive.py
